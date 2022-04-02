fcb2b4618f5a698dcbb02e541aa121df462b051b
ZacharyLeeper <zachl@cs.utexas.edu>
2022-04-02 02:53:20 -0500

    Set up student handout.

---
+++ cache/cache-ref.c
@@ -0,0 +1,305 @@
+/*
+ * cache.c - A cache simulator that can replay traces from Valgrind
+ *     and output statistics such as number of hits, misses, and
+ *     evictions.  The replacement policy is LRU.
+ *
+ * Implementation and assumptions:
+ *  1. Each load/store can cause at most one cache miss. (I examined the trace,
+ *  the largest request I saw was for 8 bytes).
+ *  2. Instruction loads (I) are ignored, since we are interested in evaluating
+ *  trans.c in terms of its data cache performance.
+ *  3. data modify (M) is treated as a load followed by a store to the same
+ *  address. Hence, an M operation can result in two cache hits, or a miss and a
+ *  hit plus an possible eviction.
+ *
+ * The function printSummary() is given to print output.
+ * Please use this function to print the number of hits, misses and evictions.
+ * This is crucial for the driver to evaluate your work.
+ */
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+#include <limits.h>
+#include <string.h>
+#include <errno.h>
+#include "cache.h"
+
+#define ADDRESS_LENGTH 64
+
+/* Counters used to record cache statistics in printSummary().
+   test-cache uses these numbers to verify correctness of the cache. */
+
+//Increment when a miss occurs
+int miss_count = 0;
+
+//Increment when a hit occurs
+int hit_count = 0;
+
+//Increment when a dirty eviction occurs
+int dirty_eviction_count = 0;
+
+//Increment when a clean eviction occurs
+int clean_eviction_count = 0;
+
+/* TODO: add more globals, structs, macros if necessary */
+uword_t next_lru;
+
+
+/*
+ * Initialize the cache according to specified arguments
+ * Called by cache-runner so do not modify the function signature
+ *
+ * The code provided here shows you how to initialize a cache structure
+ * defined above. It's not complete and feel free to modify/add code.
+ */
+cache_t *create_cache(int s_in, int b_in, int E_in, int d_in)
+{
+    /* see cache-runner for the meaning of each argument */
+    cache_t *cache = malloc(sizeof(cache_t));
+    cache->s = s_in;
+    cache->b = b_in;
+    cache->E = E_in;
+    cache->d = d_in;
+    unsigned int S = (unsigned int) pow(2, cache->s);
+    unsigned int B = (unsigned int) pow(2, cache->b);
+
+    unsigned int i, j;
+    cache->sets = (cache_set_t*) calloc(S, sizeof(cache_set_t));
+    for (i=0; i<S; i++){
+        cache->sets[i].lines = (cache_line_t*) calloc(cache->E, sizeof(cache_line_t));
+        for (j=0; j<cache->E; j++){
+            cache->sets[i].lines[j].valid = 0;
+            cache->sets[i].lines[j].tag = 0;
+            cache->sets[i].lines[j].lru = 0;
+            cache->sets[i].lines[j].dirty = 0;
+            cache->sets[i].lines[j].data = calloc(B, sizeof(byte_t));
+        }
+    }
+
+    /* TODO: add more code for initialization */
+
+    return cache;
+}
+
+cache_t *create_checkpoint(cache_t *cache) {
+    unsigned int S = (unsigned int) pow(2, cache->s);
+    unsigned int B = (unsigned int) pow(2, cache->b);
+    cache_t *copy_cache = malloc(sizeof(cache_t));
+    memcpy(copy_cache, cache, sizeof(cache_t));
+    copy_cache->sets = (cache_set_t*) calloc(S, sizeof(cache_set_t));
+    for (unsigned int i = 0; i < S; i++) {
+        copy_cache->sets[i].lines = (cache_line_t*) calloc(cache->E, sizeof(cache_line_t));
+        for (unsigned int j = 0; j < cache->E; j++) {
+            memcpy(&copy_cache->sets[i].lines[j], &cache->sets[i].lines[j], sizeof(cache_line_t));
+            copy_cache->sets[i].lines[j].data = calloc(B, sizeof(byte_t));
+            memcpy(copy_cache->sets[i].lines[j].data, cache->sets[i].lines[j].data, sizeof(byte_t));
+        }
+    }
+    return copy_cache;
+}
+
+void display_set(cache_t *cache, unsigned int set_index) {
+    unsigned int S = (unsigned int) pow(2, cache->s);
+    if (set_index < S) {
+        cache_set_t *set = &cache->sets[set_index];
+        for (unsigned int i = 0; i < cache->E; i++) {
+            printf ("Valid: %d Tag: %llx Lru: %lld Dirty: %d\n", set->lines[i].valid, set->lines[i].tag, set->lines[i].lru, set->lines[i].dirty);
+        }
+    } else {
+        printf ("Invalid Set %d. 0 <= Set < %d\n", set_index, S);
+    }
+}
+
+/*
+ * Free allocated memory. Feel free to modify it
+ */
+void free_cache(cache_t *cache)
+{
+    unsigned int i, j;
+    unsigned int S = (unsigned int) pow(2, cache->s);
+    for (i = 0; i < S; i++){
+        for (j = 0; j < cache->E; j++) {
+            free(cache->sets[i].lines[j].data);
+        }
+        free(cache->sets[i].lines);
+    }
+    free(cache->sets);
+    free(cache);
+}
+
+/* TODO:
+ * Get the line for address contained in the cache
+ * On hit, return the cache line holding the address
+ * On miss, returns NULL
+ */
+cache_line_t *get_line(cache_t *cache, uword_t addr)
+{
+    /* your implementation */
+    size_t S = (size_t)pow(2, cache->s);
+    uword_t set_index = (addr >> cache->b) & (S - 1);
+    cache_set_t *set = cache->sets + set_index;
+    uword_t tag = (addr >> (cache->b + cache->s));
+    for (int i = 0; i < cache->E; i++) {
+        if(set->lines[i].tag == tag && set->lines[i].valid) {
+            return set->lines + i;
+        }
+    }
+    return NULL;
+}
+
+/* TODO:
+ * Select the line to fill with the new cache line
+ * Return the cache line selected to filled in by addr
+ */
+cache_line_t *select_line(cache_t *cache, uword_t addr)
+{
+    /* your implementation */
+    size_t S = (size_t)pow(2, cache->s);
+    uword_t set_index = (addr >> cache->b) & (S - 1);
+    cache_set_t *set = cache->sets + set_index;
+    for (int i = 0; i < cache->E; i++) {
+        if (!set->lines[i].valid) {
+            return set->lines + i;
+        }
+    }
+
+    uword_t max_lru = next_lru;
+    int index = 0;
+    for (int i = 0; i < cache->E; i++) {
+        if(set->lines[i].lru < max_lru) {
+            index = i;
+            max_lru = set->lines[i].lru;
+        }
+    }
+
+    return set->lines + index;
+}
+
+/*  TODO:
+ * Check if the address is hit in the cache, updating hit and miss data.
+ * Return true if pos hits in the cache.
+ */
+bool check_hit(cache_t *cache, uword_t addr, operation_t operation)
+{
+    /* your implementation */
+    cache_line_t *line = get_line(cache, addr);
+    if(line != NULL) {
+        hit_count++;
+        line->lru = next_lru++;
+        line->dirty |= (operation == WRITE);
+    } else {
+        miss_count++;
+    }
+
+    return (line != NULL);
+}
+
+/*  TODO:
+ * Handles Misses, evicting from the cache if necessary.
+ * Fill out the evicted_line_t struct with info regarding the evicted line.
+ */
+evicted_line_t *handle_miss(cache_t *cache, uword_t addr, operation_t operation, byte_t *incoming_data)
+{
+    size_t B = (size_t)pow(2, cache->b);
+    size_t S = (size_t)pow(2, cache->s);
+    evicted_line_t *evicted_line = malloc(sizeof(evicted_line_t));
+    evicted_line->data = (byte_t *) calloc(B, sizeof(byte_t));
+    /* your implementation */
+    cache_line_t *line = select_line(cache, addr);
+
+    evicted_line->valid = line->valid;
+    evicted_line->dirty = line->dirty;
+
+    uword_t set_index = (addr >> cache->b) & (S - 1);
+    evicted_line->addr = (line->tag << (cache->s + cache->b)) | (set_index << cache->b);
+
+    line->lru = next_lru++;
+
+    if (line->valid) {
+        if (line->dirty) {
+            dirty_eviction_count++;
+        } else {
+            clean_eviction_count++;
+        }
+    }
+
+    line->valid = 1;
+    line->tag = (addr >> (cache->b + cache->s));
+    line->dirty = (operation == WRITE);
+    memcpy(evicted_line->data, line->data, B);
+
+    if (incoming_data != NULL)
+        memcpy(line->data, incoming_data, B);
+
+    return evicted_line;
+}
+
+/* TODO:
+ * Get a byte from the cache and write it to dest.
+ * Preconditon: pos is contained within the cache.
+ */
+void get_byte_cache(cache_t *cache, uword_t addr, byte_t *dest)
+{
+    cache_line_t *line = get_line(cache, addr);
+    size_t B = (size_t)pow(2, cache->b);
+    *dest = line->data[addr % B];
+}
+
+
+/* TODO:
+ * Get 8 bytes from the cache and write it to dest.
+ * Preconditon: pos is contained within the cache.
+ */
+void get_word_cache(cache_t *cache, uword_t addr, word_t *dest) {
+
+    /* your implementation */
+    byte_t *dest_b = (byte_t*)dest;
+    for (size_t i = 0; i < sizeof(word_t); i++) {
+        get_byte_cache(cache, addr + i, dest_b + i);
+    }
+}
+
+
+/* TODO:
+ * Set 1 byte in the cache to val at pos.
+ * Preconditon: pos is contained within the cache.
+ */
+void set_byte_cache(cache_t *cache, uword_t addr, byte_t val)
+{
+
+    /* your implementation */
+    size_t B = (size_t)pow(2, cache->b);
+    cache_line_t *line = get_line(cache, addr);
+    line->data[addr % B] = val;
+}
+
+
+/* TODO:
+ * Set 8 bytes in the cache to val at pos.
+ * Preconditon: pos is contained within the cache.
+ */
+void set_word_cache(cache_t *cache, uword_t addr, word_t val)
+{
+    byte_t *dest_b = (byte_t*)&val;
+    for (size_t i = 0; i < sizeof(word_t); i++) {
+        set_byte_cache(cache, addr + i, dest_b[i]);
+    }
+}
+
+/*
+ * Access data at memory address addr
+ * If it is already in cache, increast hit_count
+ * If it is not in cache, bring it in cache, increase miss count
+ * Also increase eviction_count if a line is evicted
+ *
+ * Called by cache-runner; no need to modify it if you implement
+ * check_hit() and handle_miss()
+ */
+void access_data(cache_t *cache, uword_t addr, operation_t operation)
+{
+    if(!check_hit(cache, addr, operation))
+        free(handle_miss(cache, addr, operation, NULL));
+}
